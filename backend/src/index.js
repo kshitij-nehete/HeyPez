abd219c919d1d9ea61522d9e6de2bae072c5d69a
kshitij-nehete <kshitijnehete@gmail.com>
2025-05-20 23:50:07 +0530

    signUp backend implementation

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,12 @@ import { connectDB } from "./lib/db.js";
 const app =  express();
 
 dotenv.config();
+
 const PORT = process.env.PORT;
 
+app.use(express.json())
 app.use("/api/auth", authRoutes)
+
 
 app.listen(PORT, () => {
     console.log("Server is running on Port:" + PORT); 
